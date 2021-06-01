45d5730f8266e234a9ec1992e4ab139922ba7b35
Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
2021-06-01 09:46:02 -0700

    expanded testing.

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -4,14 +4,24 @@
 import React from 'react';
 
 import { render, fireEvent, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 describe('App component', () => {
-  it('renders App', async () => {
+  it('renders App', () => {
     render(<App />);
-    
-    const input = await screen.getByPlaceholderText('colors');
+
+    const input = screen.getByPlaceholderText('colors');
+    const undo = screen.getByRole('button', { name: 'undo' });
+    const redo = screen.getByRole('button', { name: 'redo' });
+
     fireEvent.change(input, { target: { value: '#FF0000' } });
     expect(input.value).toBe('#ff0000');
+    fireEvent.change(input, { target: { value: '#FF69B4' } });
+    expect(input.value).toBe('#ff69b4');
+    userEvent.click(undo);
+    expect(input.value).toBe('#ff0000');
+    userEvent.click(redo);
+    expect(input.value).toBe('#ff69b4');
   });
 });
